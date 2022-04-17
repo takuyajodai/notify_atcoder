f9a72cc5e29fb36b867e5bbf0dc008a5bb6e8dfb
takuyajodai <j.takuya0625@icloud.com>
2022-04-17 14:16:04 +0900

    add /

--- main.py
+++ main.py
@@ -18,6 +18,11 @@ LINE_CHANNEL_ACCESS_TOKEN = os.environ["LINE_CHANNEL_ACCESS_TOKEN"]
 LINE_CHANNEL_SECRET = os.environ["LINE_CHANNEL_SECRET"]
 line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(LINE_CHANNEL_SECRET)
+
+@app.route("/")
+def hello_world():
+  return "hello world!"
+  
 @app.route("/callback", methods=['POST'])
 def callback():
    # get X-Line-Signature header value
